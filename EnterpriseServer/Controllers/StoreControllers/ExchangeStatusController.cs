bd71a1054afbc8b06a4aac8a2d98db921752db5e
Smorlord <nick.scheib_00@yahoo.de>
2022-02-25 22:59:18 +0100

    [ADD] ExchangeStatusController - UC8 Extension

---
+++ EnterpriseServer/Controllers/StoreControllers/ExchangeStatusController.cs
@@ -0,0 +1,54 @@
+﻿using data;
+using data.EnterpriseData;
+using data.StoreData;
+using Microsoft.AspNetCore.Mvc;
+using services.StoreServices;
+
+namespace EnterpriseServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeStatusController
+    {
+
+        private IStoreService storeService;
+
+        public ExchangeStatusController(ILogger<OrderController> logger, IStoreService storeService,) {
+            this.storeService = storeService;
+        }
+
+        [HttpPost]
+        [Route("exchange-status")]
+        public void changeExchangeStatus(int storeId)
+        {
+            using (var db = new TradingsystemDbContext())
+            {
+                Store reciever = storeService.getStore(db, storeId);
+
+                reciever.StockItems.ForEach(recieverItem =>
+                {
+                    reciever.ExchangeEntry.ForEach( exchangeEntry =>
+                    {
+                        if(recieverItem.Product.Id == exchangeEntry.Product.Id) 
+                        {
+                            recieverItem.Amount += exchangeEntry.ExchangeAmount;
+                            recieverItem.ExchangeStatus = null;
+
+                            storeService.getStore(db, exchangeEntry.StoreId).StockItems.ForEach( supplierItem => 
+                            {
+                                if(supplierItem.Product.Id == exchangeEntry.Product.Id)
+                                {
+                                    supplierItem.Amount -= exchangeEntry.ExchangeAmount;
+                                    supplierItem.ExchangeStatus = null;
+                                }
+                            });
+                        }
+                        
+                    });
+                });
+
+            }
+
+        }
+    }
+}
